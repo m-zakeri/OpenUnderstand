a3134c2f589bab211ace4f2c572a8b1c81d01021
m-zakeri <m-zakeri@live.com>
2021-10-09 22:26:01 +0330

    Add ound.py the OpenUnderstand main driver

---
+++ ound.py
@@ -0,0 +1,16 @@
+"""
+Open Understand main driver
+to create project parse tree, analyze project, and create symbol table db
+It is the same Understand und command line tool
+
+"""
+
+
+class Project():
+    # Todo: Implement project class
+    pass
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    p = Project()
