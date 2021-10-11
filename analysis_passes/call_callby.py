089ebebedf29370fc0730cce57e5f63ba7b15601
m-zakeri <m-zakeri@live.com>
2021-10-11 14:49:01 +0330

    Add call_callby.py template module

---
+++ analysis_passes/call_callby.py
@@ -0,0 +1,23 @@
+"""
+## Description
+This module find all OpenUnderstand call and callby references in a Java project
+
+
+## References
+
+
+"""
+
+__author__ = 'Morteza Zakeri'
+__version__ = '0.1.0'
+
+from gen.javaLabeled.JavaParserLabeledListener import JavaParserLabeledListener
+
+
+class CallAndCallBy(JavaParserLabeledListener):
+    """
+    #Todo: Implementing the ANTLR listener pass for Java Call and Java Callby reference kind
+    """
+    pass
+
+
