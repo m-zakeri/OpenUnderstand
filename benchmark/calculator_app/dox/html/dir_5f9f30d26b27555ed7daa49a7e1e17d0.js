711849f0c802fdf99e95cea0f62a2eb9ee634078
Ali Sedaghi <ali_sedaghi@ymail.com>
2022-04-25 22:20:07 +0430

    Remove benchmark unzipped folder

--- benchmark/calculator_app/dox/html/dir_5f9f30d26b27555ed7daa49a7e1e17d0.js
+++
@@ -1,6 +0,0 @@
-var dir_5f9f30d26b27555ed7daa49a7e1e17d0 =
-[
-    [ "Main.java", "Main_8java.html", [
-      [ "Main", "classcom_1_1calculator_1_1app_1_1init_1_1Main.html", "classcom_1_1calculator_1_1app_1_1init_1_1Main" ]
-    ] ]
-];
--- benchmark/calculator_app/dox/html/search/namespaces_0.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['display_41',['display',['../namespacecom_1_1calculator_1_1app_1_1display.html',1,'com::calculator::app']]],
-  ['init_42',['init',['../namespacecom_1_1calculator_1_1app_1_1init.html',1,'com::calculator::app']]],
-  ['method_43',['method',['../namespacecom_1_1calculator_1_1app_1_1method.html',1,'com::calculator::app']]]
-];
