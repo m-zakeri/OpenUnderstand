4b88380ec6b13b85276757caf99669a768cc88db
Ali Sedaghi <ali_sedaghi@ymail.com>
2022-04-30 01:05:33 +0430

    Finish implementing java modify deref partial reference

--- openunderstand/modify_modifyby.py
+++ openunderstand/modify_modifyby.py
@@ -1,208 +1,194 @@
-import os
 from antlr4 import *
-from gen.javaLabeled.JavaLexer import JavaLexer
 from gen.javaLabeled.JavaParserLabeled import JavaParserLabeled
 from gen.javaLabeled.JavaParserLabeledListener import JavaParserLabeledListener
-from oudb.fill import main as db_fill
-from oudb.api import create_db, open as db_open
 from oudb.models import KindModel, EntityModel, ReferenceModel
+from import_importby import Project, get_parse_tree, get_project_info, get_kind_name, get_parent
 
-PRJ_INDEX = 0
-PROJECTS_NAME = [
-    'calculator_app',
-    'JSON',
-    'testing_legacy_code',
-    'jhotdraw-develop',
-    'xerces2j',
-    'jvlt-1.3.2',
-    'jfreechart',
-    'ganttproject',
-    '105_freemind',
-]
-PROJECT_NAME = PROJECTS_NAME[PRJ_INDEX]
-DB_PATH = f"../../databases/modify/{PROJECT_NAME}.oudb"
-PROJECT_PATH = f"../../benchmarks/{PROJECT_NAME}"
+PRJ_INDEX = 8
+REF_NAME = "modify"
 
 
-class Project:
-    def __init__(self, db_name, project_dir, project_name=None):
-        self.db_name = db_name
-        self.project_dir = project_dir
-        self.project_name = project_name
-        self.file_paths = []
-        self.file_names = []
-
-    def init_db(self):
-        create_db(self.db_name, self.project_dir, self.project_name)
-        db_fill()
-        db_open(self.db_name)
-
-    def get_java_files(self):
-        for dir_path, _, file_names in os.walk(self.project_dir):
-            for file in file_names:
-                if '.java' in str(file):
-                    path = os.path.join(dir_path, file)
-                    path = path.replace("/", "\\")
-                    self.file_paths.append(path)
-                    self.file_names.append(file)
-                    add_java_file_entity(path, file)
-
-    def get_parent(self, parent_file_name):
-        parent_file_index = self.file_names.index(parent_file_name)
-        parent_file_path = self.file_paths[parent_file_index]
-        parent_entity = EntityModel.get_or_none(
-            _kind=KindModel.get_or_none(_name="Java File")._id,
-            _name=parent_file_name,
-            _longname=parent_file_path,
-        )
-        return parent_entity
+def get_prefixes(ctx, ctx_type):
+    branches = ctx.parentCtx.children
+    prefixes = ""
+    for branch in branches:
+        if type(branch).__name__ == ctx_type:
+            break
+        prefixes += branch.getText() + " "
+    return prefixes
 
 
-class ClassEntityListener(JavaParserLabeledListener):
-    def __init__(self):
-        self.class_body = None
+class ClassListener(JavaParserLabeledListener):
+    def __init__(self, files, file_name):
+        self.files = files
+        self.file_name = file_name
 
     def enterClassDeclaration(self, ctx: JavaParserLabeled.ClassDeclarationContext):
-        self.class_body = ctx.getText()
+        parent_entity, parent_file_path = get_parent(self.file_name, self.files)
+        prefixes = get_prefixes(ctx, "ClassDeclarationContext")
+        kind_name = get_kind_name(prefixes, kind="Class")
+        obj, _ = EntityModel.get_or_create(
+            _kind=KindModel.get_or_none(_name=kind_name).get_id(),
+            _parent=parent_entity.get_id(),
+            _name=ctx.IDENTIFIER().getText(),
+            _longname=parent_file_path,
+            _contents=ctx.getText(),
+        )
+
+
+class VariableDeclaratorListener(JavaParserLabeledListener):
+    def enterVariableDeclarators(self, ctx: JavaParserLabeled.VariableDeclaratorContext):
+        print(ctx.getText())
 
 
 class ModifyListener(JavaParserLabeledListener):
-    def __init__(self, file_names):
-        self.repository = []
-        self.scope = None
-        self.scope_info = []
-        self.line = None
-        self.column = None
-        self.file_names = file_names
-        self.ent = None
+    def __init__(self, files, file_name):
+        self.files = files
+        self.file_name = file_name
 
-    def make_scope_method(self, current):
-        content = current.getText()
-        name = current.IDENTIFIER().getText()
-        if type(current).__name__ == "MethodDeclarationContext":
-            if "static" in current.getText():
-                kind = KindModel.get_or_none(_name="Java Static Method Public Member")._id
-            else:
-                kind = KindModel.get_or_none(_name="Java Method Public Member")._id
-        current = current.parentCtx
-        while current is not None:
-            if type(current).__name__ == "ClassDeclarationContext":
-                parent = self.make_scope_class(current)
-                break
-            current = current.parentCtx
-        self.scope_info.append({"parent": parent, "kind": kind, "line": self.line, "column": self.column,
-                                "name": name, "content": content, "scope": self.scope})
-
-    def make_scope_class(self, current):
-        content = current.getText()
-        name = current.IDENTIFIER().getText()
-        kind = KindModel.get_or_none(_name="Java Class Type Public Member")._id
-        parent = "file"
-        return {"parent": parent, "kind": kind, "name": name, "content": content}
-
-    def search_scope(self, ctx):
+    @staticmethod
+    def search_scope(ctx, type_names):
+        # Traverse bottom up until reaching a class or method
         current = ctx.parentCtx
         while current is not None:
-            if type(current).__name__ == "ClassDeclarationContext" or type(
-                    current).__name__ == "MethodDeclarationContext":
-                self.scope = current.IDENTIFIER().getText()
-                if type(current).__name__ == "ClassDeclarationContext":
-                    self.scope_info.append(self.make_scope_class(current))
-                elif type(current).__name__ == "MethodDeclarationContext":
-                    self.make_scope_method(current)
-                return
+            type_name = type(current).__name__
+            if type_name in type_names:
+                return current
             current = current.parentCtx
-        self.scope = " "
+        return None
+
+    def make_scope_class(self, ctx, file_name):
+        prefixes = get_prefixes(ctx, "ClassDeclarationContext")
+        kind_name = get_kind_name(prefixes, kind="Class")
+        kind_id = KindModel.get_or_none(_name=kind_name).get_id()
+        name = ctx.IDENTIFIER().getText()
+        parent_entity, parent_file_path = get_parent(file_name, self.files)
+        content = ctx.getText()
+
+        obj = EntityModel.get_or_none(
+            _kind=KindModel.get_or_none(_name=kind_name).get_id(),
+            _parent=parent_entity.get_id(),
+            _name=name,
+            _longname=parent_file_path,
+        )
+        return {
+            "id": obj.get_id(),
+            "kind_id": kind_id,
+            "parent_id": parent_entity.get_id(),
+            "name": name,
+            "longname": parent_file_path,
+            "content": content
+        }
+
+    def make_scope_method(self, ctx, file_name):
+        prefixes = get_prefixes(ctx, "MethodDeclarationContext")
+        kind_name = get_kind_name(prefixes, kind="Method")
+        kind_id = KindModel.get_or_none(_name=kind_name).get_id()
+        name = ctx.IDENTIFIER().getText()
+        content = ctx.getText()
+        parent_ctx = self.search_scope(ctx, ["ClassDeclarationContext"])
+        parent_entity = self.make_scope_class(parent_ctx, file_name)
+
+        obj, _ = EntityModel.get_or_create(
+            _kind=kind_id,
+            _parent=parent_entity['id'],
+            _name=name,
+            _longname=f"{parent_entity['name']}.{name}",
+            _contents=content,
+        )
+        return {
+            "id": obj.get_id(),
+            "kind_id": kind_id,
+            "parent_id": parent_entity['id'],
+            "name": name,
+            "longname": f"{parent_entity['name']}.{name}",
+            "content": content
+        }
 
     def enterExpression6(self, ctx: JavaParserLabeled.Expression6Context):
-        line_col = str(ctx.children[0].start).split(",")[3][:-1].split(':')
-        self.line = line_col[0]
-        self.column = line_col[1]
-        self.search_scope(ctx)
+        self.modify_deref_partial(ctx)
 
     def enterExpression21(self, ctx: JavaParserLabeled.Expression21Context):
-        operations = ['+=', '-=', '/=', '*=', '&=', '|=', '^=', '%=']
-        if ctx.children[1].getText() in operations:
-            self.search_scope(ctx)
-            line_col = str(ctx.children[0].start).split(",")[3][:-1].split(':')
-            self.line = line_col[0]
-            self.column = line_col[1]
+        self.modify_deref_partial(ctx)
+
+    def modify_deref_partial(self, ctx):
+        lhs_text = ctx.children[0].getText()
+        scope = None
+        if "[" in lhs_text and "]" in lhs_text:
+            var_name = ctx.children[0].children[0].getText()
+            var_entity = add_var_entity(var_name)
+
+            line, col = str(ctx.start).split(",")[3][:-1].split(':')
+
+            file_entity, _ = get_parent(self.file_name, self.files)
+            file_id = file_entity.get_id()
+
+            ref_dict = {'line': line, 'column': col, 'file_id': file_id, 'text': ctx.getText()}
+
+            scope_ctx = self.search_scope(ctx, ["ClassDeclarationContext", "MethodDeclarationContext"])
+            if type(scope_ctx).__name__ == "ClassDeclarationContext":
+                scope = self.make_scope_class(scope_ctx, self.file_name)
+            elif type(scope_ctx).__name__ == "MethodDeclarationContext":
+                scope = self.make_scope_method(scope_ctx, self.file_name)
+
+            add_references(scope, var_entity, ref_dict)
 
 
-def create_Entity(name, longname, parent, contents, kind, value, entity_type):
-    obj, has_created = EntityModel.get_or_create(_kind=kind,
-                                                 _parent=parent,
-                                                 _name=name,
-                                                 _longname=longname,
-                                                 _value=value,
-                                                 _type=entity_type,
-                                                 _contents=contents
-                                                 )
-    return obj
-
-
-def create_Ref(kind, file, line, column, ent, scope):
-    obj, has_created = ReferenceModel.get_or_create(_kind=kind,
-                                                    _file=file,
-                                                    _line=line,
-                                                    _column=column,
-                                                    _ent=ent,
-                                                    _scope=scope
-                                                    )
-    return obj
-
-
-def get_parse_tree(file_path):
-    file = FileStream(file_path)
-    lexer = JavaLexer(file)
-    tokens = CommonTokenStream(lexer)
-    parser = JavaParserLabeled(tokens)
-    return parser.compilationUnit()
-
-
-def add_java_file_entity(file_path, file_name):
-    kind_id = KindModel.get_or_none(_name="Java File")._id
+def add_var_entity(var_name):
     obj, _ = EntityModel.get_or_create(
-        _kind=kind_id,
-        _name=file_name,
-        _longname=file_path,
-        _contents=FileStream(file_path),
+        _kind=152,  # Java Unresolved Variable,
+        _name=var_name,
+        _longname=var_name,
     )
-    return obj
+    return {
+        "id": obj.get_id(),
+        "kind_id": 152,
+        "name": var_name,
+        "longname": var_name,
+    }
 
 
-def add_references(modifying_ent, ref_dict):
+def add_references(scope, ent, ref_dict):
     ref, _ = ReferenceModel.get_or_create(
-        _kind=KindModel.get_or_none(_name="Java Modify")._id,
-        _file=modifying_ent._id,
+        _kind=208,  # Java Modify
+        _file=ref_dict['file_id'],
         _line=ref_dict['line'],
         _column=ref_dict['column'],
-        _ent=modifying_ent._id,
-        _scope=ref_dict['scope'],
+        _ent=ent['id'],
+        _scope=scope['id'],
     )
     inverse_ref, _ = ReferenceModel.get_or_create(
-        _kind=KindModel.get_or_none(_name="Java Modifyby")._id,
-        _file=modifying_ent._id,
+        _kind=209,  # Java Modifyby
+        _file=ref_dict['file_id'],
         _line=ref_dict['line'],
         _column=ref_dict['column'],
-        _ent=ref_dict['scope'],
-        _scope=modifying_ent._id,
+        _ent=scope['id'],
+        _scope=ent['id'],
     )
+
+    # print(ref_dict['text'])
+    # print(f'1. ref name: Java Modify Deref Partial')
+    # print(f'2. ref scope: {scope["longname"]} || kind: {KindModel.get_or_none(_id=scope["kind_id"])._name}')
+    # print(f'3. ref ent: {ent["longname"]} || kind: Java Variable')
+    # print(f'4. file location: {EntityModel.get_or_none(_id=ref_dict["file_id"])} || line: {ref_dict["line"]}')
+    # print("-" * 25)
 
 
 def main():
-    p = Project(DB_PATH, PROJECT_PATH, PROJECT_NAME)
+    info = get_project_info(PRJ_INDEX, REF_NAME)
+    p = Project(info['DB_PATH'], info['PROJECT_PATH'], info['PROJECT_NAME'])
     p.init_db()
     p.get_java_files()
-    for file_path, file_name in zip(p.file_paths, p.file_names):
-        modifying_entity = add_java_file_entity(file_path, file_name)
 
+    for file_name, file_path in p.files:
         tree = get_parse_tree(file_path)
-        listener = ModifyListener(p.file_names)
         walker = ParseTreeWalker()
+
+        class_listener = ClassListener(p.files, file_name)
+        listener = ModifyListener(p.files, file_name)
+
+        walker.walk(class_listener, tree)
         walker.walk(listener, tree)
-        for i in listener.scope_info:
-            add_references(modifying_entity, i)
 
 
 if __name__ == '__main__':
