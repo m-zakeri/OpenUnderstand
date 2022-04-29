4b7d7b3f92dacf9011857167e1957dec25f0cec7
Ali Sedaghi <ali_sedaghi@ymail.com>
2022-04-29 22:50:35 +0430

    Refactor understand tester

--- openunderstand/g10_tests.py
+++ openunderstand/g10_tests.py
@@ -1,44 +1,21 @@
 import understand as und
+from import_importby import get_project_info
 
-PRJ_INDEX = -1
-PROJECTS_NAME = [
-    'calculator_app',
-    'JSON',
-    'testing_legacy_code',
-    'jhotdraw-develop',
-    'xerces2j',
-    'jvlt-1.3.2',
-    'jfreechart',
-    'ganttproject',
-    '105_freemind',
-    'custom_app',
-]
-PROJECT_NAME = PROJECTS_NAME[PRJ_INDEX]
-DB_PATH = f"../../databases/origin/{PROJECT_NAME}.udb"
-PROJECT_PATH = f"../../benchmarks/{PROJECT_NAME}"
+PRJ_INDEX = 0
+REF_NAME = "origin"
 
 
 def test_understand_kinds():
-    db = und.open(DB_PATH)
+    info = get_project_info(PRJ_INDEX, REF_NAME)
+    db = und.open(info['DB_PATH'])
     for ent in db.ents():
-        for ref in ent.refs("Modify"):
-            print(f'ref.scope (entity performing reference)\t: "{ref.scope().longname()}", kind: "{ref.scope().kind()}"')
-            print(f'ref.ent (entity being referenced)\t\t: "{ref.ent().longname()}", kind: "{ref.ent().kind()}"')
-            print(f'File where the reference occurred: "{ref.file().longname()}", line: {ref.line()}, col: {ref.column()}')
+        for ref in ent.refs("Import"):
+            print(f'1. ref name: {ref.kindname()} || inverse ref name: {ref.kind().inv().longname()}')
+            print(f'2. ref scope: {ref.scope().longname()} || kind: {ref.scope().kind()}')
+            print(f'3. ref ent: {ref.ent().longname()} || kind: {ref.ent().kind()}')
+            print(f'4. file location: {ref.file().longname()} || line: {ref.line()}')
+            print("-" * 25)
 
-            print(f"Entity longname: {ent.longname()}")
-            print(f"Entity parent: {ent.parent()}")
-            print(f"Entity kind: {ent.kind()}")
-            print(f"Entity value: {ent.value()}")
-            print(f"Entity type: {ent.type()}")
-            print(f"Entity contents: {ent.contents()}")
-            print('-' * 50)
-
-            print(f"File kind: {ref.file().kind()}")
-            print(f"Parent: {ref.file().parent()}, long name: {ref.file().longname()}")
-            print(f"Value: {ref.file().value()}, type: {ref.file().type()}")
-            print(f"Contents: {ref.file().contents()}, name: {ref.file().name()}")
-            print('-' * 50)
 
 if __name__ == '__main__':
     test_understand_kinds()
