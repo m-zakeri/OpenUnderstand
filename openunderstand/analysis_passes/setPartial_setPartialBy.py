8c907c139125369712096c5109a076e2cfd9028c
Soheil Hoseini <soheil.hosseini78@gmail.com>
2022-05-06 23:16:53 +0430

    Revert "Update setPartial_setPartialBy.py"
    This reverts commit 7b87750163f0105d724caecd44ef93f9443c15a1.

--- openunderstand/analysis_passes/setPartial_setPartialBy.py
+++ openunderstand/analysis_passes/setPartial_setPartialBy.py
@@ -11,19 +11,65 @@ This module find all OpenUnderstand call and callby references in a Java project
 
 """
 
-__author__ = 'Mohammad ali Samadi'
+__author__ = 'Soheil Hoseini'
 __version__ = '0.1.0'
 
 from gen.javaLabeled.JavaParserLabeledListener import JavaParserLabeledListener
 from gen.javaLabeled.JavaParserLabeled import JavaParserLabeled
 import analysis_passes.class_properties as class_properties
 
-class Setpartial_Setpartialby(JavaParserLabeledListener):
+
+class SetPartialSetByPartial(JavaParserLabeledListener):
+
     def __init__(self):
+
+        ############ Soheil
+        self.currentMethod = ""
+        self.currentClass = ""
+        self.partialsList = []
+        self.fieldName = ""
+        self.instanceName = ""
+
+        ############################ Ali
         self.Currentmethod = ""
         self.setpartial = []
         self.dict = []
 
+    ########################## Soheil
+    @property
+    def getPartialsList(self):
+        return self.partialsList
+
+    # Save the name of the current method
+    def enterMethodDeclaration(self, ctx: JavaParserLabeled.MethodDeclarationContext):
+        self.currentMethod = ctx.IDENTIFIER().getText()
+
+    # Save the name of the field and the instance
+    def enterExpression1(self, ctx: JavaParserLabeled.Expression1Context):
+        self.fieldName = ctx.IDENTIFIER().getText()
+
+        # Check if we have a field assignment
+        if ctx.DOT().getText() == ".":
+            self.instanceName = ctx.expression().getText()
+
+        # Check the existence of the record before adding it to the list
+        tmpDict = {"currentClass": self.currentClass, "currentMethod": self.currentMethod,
+                   "instanceName": self.instanceName, "fieldName": self.fieldName}
+
+        alreadyExists = False
+        for dict in self.partialsList:
+            if dict["currentClass"] == self.currentClass and dict["currentMethod"] == self.currentMethod \
+                    and dict["fieldName"] != "":
+                alreadyExists = True
+
+        if not alreadyExists:
+            self.partialsList.append(tmpDict)
+
+    # Save the name of the current class
+    def enterClassDeclaration(self, ctx: JavaParserLabeled.ClassDeclarationContext):
+        self.currentClass = ctx.IDENTIFIER().getText()
+
+    ############################ ALi
     @property
     def get_dict(self):
         return self.dict
@@ -36,9 +82,8 @@ class Setpartial_Setpartialby(JavaParserLabeledListener):
     def get_setpartial(self):
         return self.setpartial
 
-    def enterMethodDeclaration(self, ctx: JavaParserLabeled.MethodDeclarationContext):
-        self.Currentmethod = ctx.IDENTIFIER().getText()
-
     def enterExpression2(self, ctx: JavaParserLabeled.Expression21Context):
-        self.setpartial.append(self, ctx.getChild(0).IDENTIFIER().getText())
-        self.dict.append(self, dict(zip(self.Currentmethod, self.setpartial)))
+        # self.setpartial.append(self, ctx.getChild(0).IDENTIFIER().getText())
+        # self.dict.append(self, dict(zip(self.Currentmethod, self.setpartial)))
+        pass
+
