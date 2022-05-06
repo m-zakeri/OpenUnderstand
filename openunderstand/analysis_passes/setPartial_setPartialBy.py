6a9658bfb993c36329772268e27d4b0e1fc79023
Soheil Hoseini <soheil.hosseini78@gmail.com>
2022-05-06 22:28:50 +0430

    set partial for instance detection completed

--- openunderstand/analysis_passes/setPartial_setPartialBy.py
+++ openunderstand/analysis_passes/setPartial_setPartialBy.py
@@ -22,11 +22,20 @@ import analysis_passes.class_properties as class_properties
 class SetPartialSetByPartial(JavaParserLabeledListener):
 
     def __init__(self):
+
+        ############ Soheil
         self.currentMethod = ""
-        self.sourceClass = ""
         self.currentClass = ""
         self.partialsList = []
+        self.fieldName = ""
+        self.instanceName = ""
 
+        ############################ Ali
+        self.Currentmethod = ""
+        self.setpartial = []
+        self.dict = []
+
+    ########################## Soheil
     @property
     def getPartialsList(self):
         return self.partialsList
@@ -35,19 +44,46 @@ class SetPartialSetByPartial(JavaParserLabeledListener):
     def enterMethodDeclaration(self, ctx: JavaParserLabeled.MethodDeclarationContext):
         self.currentMethod = ctx.IDENTIFIER().getText()
 
-    # Check if we have a field assignment
-    def enterExpression21(self, ctx: JavaParserLabeled.Expression21Context):
-        if ctx.ASSIGN() == "=" and ctx.getChild(0).Dot() == "0":
-            instanceName = ctx.getChild(0).getChild(0).getChild(0).IDENTIFIER().getText()
-            fieldName = ctx.getChild(0).IDENTIFIER().getText()
-            self.partialsList.append({"currentMethod": self.currentMethod, "sourceClass": self.sourceClass
-                                         , "currentClass": self.currentClass, "instanceName": instanceName
-                                         , "fieldName": fieldName})
+    # Save the name of the field and the instance
+    def enterExpression1(self, ctx: JavaParserLabeled.Expression1Context):
+        self.fieldName = ctx.IDENTIFIER().getText()
 
-    # Save the name of the source class
-    def enterClassOrInterfaceType(self, ctx: JavaParserLabeled.ClassOrInterfaceTypeContext):
-        self.sourceClass = ctx.IDENTIFIER().getText()
+        # Check if we have a field assignment
+        if ctx.DOT().getText() == ".":
+            self.instanceName = ctx.expression().getText()
+
+        # Check the existence of the record before adding it to the list
+        tmpDict = {"currentClass": self.currentClass, "currentMethod": self.currentMethod,
+                   "instanceName": self.instanceName, "fieldName": self.fieldName}
+
+        alreadyExists = False
+        for dict in self.partialsList:
+            if dict["currentClass"] == self.currentClass and dict["currentMethod"] == self.currentMethod \
+                    and dict["fieldName"] != "":
+                alreadyExists = True
+
+        if not alreadyExists:
+            self.partialsList.append(tmpDict)
 
     # Save the name of the current class
     def enterClassDeclaration(self, ctx: JavaParserLabeled.ClassDeclarationContext):
         self.currentClass = ctx.IDENTIFIER().getText()
+
+    ############################ ALi
+    @property
+    def get_dict(self):
+        return self.dict
+
+    @property
+    def get_Currentmethod(self):
+        return self.Currentmethod
+
+    @property
+    def get_setpartial(self):
+        return self.setpartial
+
+    def enterExpression2(self, ctx: JavaParserLabeled.Expression21Context):
+        # self.setpartial.append(self, ctx.getChild(0).IDENTIFIER().getText())
+        # self.dict.append(self, dict(zip(self.Currentmethod, self.setpartial)))
+        pass
+
