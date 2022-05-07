4dcfffa94be347de16196e4d5dbebc29ca5d00cc
safari <mojtabasafari19@gmail.com>
2022-05-07 12:12:52 +0430

    edit main

--- benchmark/calculator_app/dox/html/classcom_1_1calculator_1_1app_1_1display_1_1print__success.js
+++
@@ -1,4 +0,0 @@
-var classcom_1_1calculator_1_1app_1_1display_1_1print__success =
-[
-    [ "print_success_message", "classcom_1_1calculator_1_1app_1_1display_1_1print__success.html#a861967d05bb884548daac43558a5b766", null ]
-];
--- benchmark/calculator_app/dox/html/search/all_4.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['integral_14',['integral',['../classcom_1_1calculator_1_1app_1_1method_1_1integral.html',1,'com::calculator::app::method']]],
-  ['integral_2ejava_15',['integral.java',['../integral_8java.html',1,'']]]
-];
