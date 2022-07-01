2563ddf4720df52c9fa3b051d27fd087edbbba12
mohammad <mohammadym7899@gmail.com>
2022-07-02 02:43:55 +0430

    Edit line of codes to fix pep8 warnings in usemodule file

--- openunderstand/analysis_passes/usemodule_usemoduleby_g11.py
+++ openunderstand/analysis_passes/usemodule_usemoduleby_g11.py
@@ -28,23 +28,34 @@ class UseModuleUseModuleByListener(JavaParserLabeledListener):
     def enterAnnotation(self, ctx:JavaParserLabeled.AnnotationContext):
         line_col = str(ctx.start).split(",")[3][:-1].split(':')
         self.useModules.append({
-            "scope": None, "ent": None, "name": ctx.children[1].IDENTIFIER()[0].getText(),
-            "line": line_col[0], "col": line_col[1]
+            "scope": None,
+            "ent": None,
+            "name": ctx.children[1].IDENTIFIER()[0].getText(),
+            "line": line_col[0],
+            "col": line_col[1]
         })
         self.useUnresolvedModules.append({
-            "scope": None, "ent": None, "name": ctx.children[1].IDENTIFIER()[0].getText(),
-            "line": line_col[0], "col": line_col[1]
+            "scope": None,
+            "ent": None,
+            "name": ctx.children[1].IDENTIFIER()[0].getText(),
+            "line": line_col[0],
+            "col": line_col[1]
         })
 
     def enterPackageDeclaration(self, ctx:JavaParserLabeled.PackageDeclarationContext):
         package_name_array = ctx.getText().replace('package', '').split('.')
         if len(package_name_array) == 4 and package_name_array[0] == 'com':
             self.useUnknownModules.append({
-                "scope": None, "ent": ctx.getChild(1).IDENTIFIER()[3].getText(), "name":ctx.getChild(1).IDENTIFIER()[2].getText(),
-                "line": 1, "col": 1
+                "scope": None,
+                "ent": ctx.getChild(1).IDENTIFIER()[3].getText(),
+                "name": ctx.getChild(1).IDENTIFIER()[2].getText(),
+                "line": 1,
+                "col": 1
             })
             self.useUnresolvedModules.append({
-                "scope": None, "ent": ctx.getChild(1).IDENTIFIER()[3].getText(),
+                "scope": None,
+                "ent": ctx.getChild(1).IDENTIFIER()[3].getText(),
                 "name": ctx.getChild(1).IDENTIFIER()[2].getText(),
-                "line": 1, "col": 1
+                "line": 1,
+                "col": 1
             })
