059709e4127998bee23dcbd5e55a5d369a163f5d
mohammad <mohammadym7899@gmail.com>
2022-07-02 02:49:22 +0430

    Fix pep8 warning for distance rules in modify file

--- openunderstand/analysis_passes/modify_modifyby_g11.py
+++ openunderstand/analysis_passes/modify_modifyby_g11.py
@@ -1,6 +1,6 @@
 """
 ## Description
-This module find all OpenUnderstand call and callby references in a Java project
+This module find all OpenUnderstand modify and modifyby references in a Java project
 
 ## References
 
@@ -15,23 +15,23 @@ from openunderstand.gen.javaLabeled.JavaParserLabeled import JavaParserLabeled
 
 class ModifyModifyByListener(JavaParserLabeledListener):
     """
-    #Todo: Implementing the ANTLR listener pass for Java Call and Java Callby reference kind
+    #Todo: Implementing the ANTLR listener pass for Java modify and Java modifyby reference kind
     """
     modifyBy = []
     scope = None
     ent = None
     isE7 = False
 
-    def enterClassDeclaration(self, ctx:JavaParserLabeled.ClassDeclarationContext):
+    def enterClassDeclaration(self, ctx: JavaParserLabeled.ClassDeclarationContext):
         self.scope = ctx.IDENTIFIER().getText()
 
     def enterMethodDeclaration(self, ctx: JavaParserLabeled.MethodDeclarationContext):
         self.scope = ctx.IDENTIFIER().getText()
 
-    def enterExpression0(self, ctx:JavaParserLabeled.Expression0Context):
+    def enterExpression0(self, ctx: JavaParserLabeled.Expression0Context):
         self.ent = ctx.getText()
 
-    def exitExpression0(self, ctx:JavaParserLabeled.Expression0Context):
+    def exitExpression0(self, ctx: JavaParserLabeled.Expression0Context):
         if self.isE7:
             line_col = str(ctx.children[1].start).split(",")[3][:-1].split(':')
 
