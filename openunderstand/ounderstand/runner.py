7999acfd2077cb4a037727c5299f2342d67a7b0e
Mammad_S.Parshkuh <100087317+YAYA-star1380@users.noreply.github.com>
2024-02-08 00:16:57 +0330

    Add files via upload

--- openunderstand/ounderstand/runner.py
+++ openunderstand/ounderstand/runner.py
@@ -1,10 +1,11 @@
-from multiprocessing import cpu_count, Pool
+# from multiprocessing import cpu_count, Pool
 from ounderstand.parsing_process import process_file, get_files
-
-
 def runner(path_project: str = ""):
     files = get_files(path_project)
-    with Pool(cpu_count()) as pool:
-        pool.map_async(process_file, files)
-        pool.close()
-        pool.join()
+    for item in files:
+        process_file(item)
+    # with Pool(cpu_count()) as pool:
+    # pool.map_async(process_file, files)
+    # pool.close()
+    # pool.join()
+
