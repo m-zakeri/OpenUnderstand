bde90d5662eb5d569d4358e2cc821add4322c847
Amin Zarei <amin.hasan.zarei@gmail.com>
2024-04-19 11:15:50 +0330

    add some metrics

--- test_openunderstand.py
+++ test_openunderstand.py
@@ -7,10 +7,19 @@ sys.path.append(join(getcwd(), "openunderstand", "oudb"))
 sys.path.append(join(getcwd(), "openunderstand", "utils"))
 from openunderstand.ounderstand.openunderstand import *
 
+# for codart
 start_parsing(
-    repo_address=join(getcwd(), "benchmark", "JSON"),
-    db_address=getcwd(),
+    repo_address="/home/y/Desktop/CodART/benchmark_projects/JSON20201115",
+    db_address="/home/y/Desktop/CodART/benchmark_projects/JSON20201115",
     db_name="mydb.udb",
     engine_core="Python3",
-    log_address=join(getcwd(), "a.log"),
+    log_address="/home/y/Desktop/CodART/benchmark_projects/JSON20201115/a.log",
 )
+# for local
+# start_parsing(
+#     repo_address=join(getcwd(), "benchmark", "JSON"),
+#     db_address=getcwd(),
+#     db_name="mydb.udb",
+#     engine_core="Python3",
+#     log_address=join(getcwd(), "a.log"),
+# )
